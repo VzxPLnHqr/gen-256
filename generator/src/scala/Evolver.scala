9500d7f016461a7552056dbc12eca8d64ea64964
VzxPLnHqr <VzxPLnHqr@proton.me>
2022-12-08 18:29:24 +0000

    parallel traversals help a lot

--- generator/src/scala/Evolver.scala
+++ generator/src/scala/Evolver.scala
@@ -17,26 +17,28 @@ trait Genetic[A]{
 object Evolver {
     def iterate[A](fitness: A => IO[BigInt])
                   (pop: List[ByteVector], newPopSize: Int)
-                  (implicit g: Genetic[A], random: std.Random[IO]): IO[List[ByteVector]] = {
+                  (implicit genetic: Genetic[A], randomIO: std.Random[IO]): IO[List[ByteVector]] = for {
+        
+        // apply fitness function to each member of population
+        // returns List[A,BigInt]
+        scoredPop <- pop.parTraverse{ 
+                        candidate_bytes => for {
+                            repr <- IO(genetic.fromBytes(candidate_bytes))
+                            score <- fitness(repr)
+                        } yield (candidate_bytes,score)
+                    }
 
-        val scoredPop = pop.traverse{ 
-            bs => for {
-                b <- IO(bs)
-                repr <- IO(g.fromBytes(bs))
-                score <- fitness(repr)
-            } yield (b,score)
-        }
-
-        //predetermined population size
-        (1 to newPopSize).toList.traverse{_ => 
-            for {
-                lhs <- scoredPop.flatMap(sampleFromWeightedList(_))
-                rhs <- scoredPop.flatMap(sampleFromWeightedList(_))
-                crossed <- crossover(lhs,rhs)(random)
-                mutated <- mutate(crossed)(random)
-            } yield mutated
-        }
-    }
+        // now build a new population by sampling from the old one
+        // predetermined/fixed population size
+        newPop <- (1 to newPopSize).toList.parTraverse{ 
+                    i => for {
+                        candidates <- sampleFromWeightedList(scoredPop)(randomIO)
+                                        .both(sampleFromWeightedList(scoredPop)(randomIO))
+                        crossed <- crossover(candidates._1,candidates._2)(randomIO)
+                        mutated <- mutate(crossed)(randomIO)
+                    } yield mutated
+                }
+    } yield newPop
 
     /**
      *  Random selection from weighted list.
@@ -50,13 +52,14 @@ object Evolver {
       * @param pop
       * @return
       */
-    def sampleFromWeightedList[A]( pop: List[(A,BigInt)]): IO[A] =
+    def sampleFromWeightedList[A]( pop: List[(A,BigInt)])
+                                 (implicit randomIO: std.Random[IO]): IO[A] =
         pop.foldLeftM((BigInt(0),Option.empty[A])){ 
             case ((accum_score, incumbent), (candidate, candidate_score)) => 
                 for {
                     new_accum_score <- IO(accum_score + candidate_score)
                     p <- IO(Real(candidate_score) / Real(new_accum_score)).map(_.toDouble)
-                    winner <- std.Random.scalaUtilRandom[IO].flatMap(_.nextDouble).map(_ <= p)
+                    winner <- randomIO.nextDouble.map(_ <= p)
                                 .ifM(
                                     ifTrue = IO(Some(candidate)),
                                     ifFalse = IO(incumbent)
@@ -64,33 +67,17 @@ object Evolver {
                 } yield (new_accum_score, winner)
         }.flatMap(r => IO.fromOption(r._2)(new RuntimeException("No candidate selected!")))
 
-    type RouletteWheel = Double => Option[ByteVector]
     /**
-     * take a scored population, make a roulette wheel with odds for a
-     * candidate proportional to what it contributes to the total fitness
-     * of the population */
-    def rouletteWheel(pop: List[(ByteVector, BigInt)]): IO[RouletteWheel] = for {
-        totalFitness <- IO(pop.map(_._2).foldLeft(BigInt(0))(_ + _))
-        popPmf <- IO(pop.map((candidate, score) => (candidate, (Real(score)/Real(totalFitness)).toDouble)).sortBy(_._2))
-        popCdf <- IO(popPmf.map(_._2).scanLeft(0.0)(_ + _).drop(1))
-        thewheel <- IO(
-            (p: Double) => popCdf.zip(popPmf).dropWhile{
-                case (accum_p, (c_bytes, c_p)) => accum_p < p
-            }.map(_._2._1).headOption
-        )
-    } yield thewheel
-
-    /**
-     * perform randome crossover between two byte vectors:
+     * perform random naive crossover between two byte vectors:
         1. pick a point on the lhs
         2. pick a point on the rhs
         3. take the tail of rhs and head of lhs
      * */
     def crossover(lhs: ByteVector, rhs: ByteVector)
         (implicit random: std.Random[IO]): IO[ByteVector] = for {
-            a <- random.betweenInt(0,lhs.size.toInt + 1)
-            b <- random.betweenInt(0,rhs.size.toInt + 1)
-            bytes = lhs.take(a) ++ rhs.drop(b)
+            r <- random.betweenInt(0,lhs.size.toInt + 1)
+                    .both(random.betweenInt(0,rhs.size.toInt + 1))
+            bytes <- IO(lhs.take(r._1)).map(_ ++ rhs.drop(r._2))
         } yield bytes
 
     /**
@@ -98,14 +85,16 @@ object Evolver {
      * to the length of the gnome */
     def mutate(input: ByteVector)(implicit random: std.Random[IO]): IO[ByteVector] = {
         val probOfMutation = if(input.size == 0) 0 else 1.0 / input.size.toDouble
-        
+
+        // note: this code does not currently ever "delete" a byte from the genome
+        // but it probably should occassionally do that
         def mutateByte(p: Double, b: Byte): IO[Byte] = 
             random.nextDouble.map(_ <= p)
             .flatMap{ 
                 case true => random.nextBytes(1).map(_.head)
                 case false => IO(b)
             }
-        input.toArray.toList.traverse(b => mutateByte(probOfMutation,b)).map(ByteVector(_))
+        input.toArray.toList.parTraverse(b => mutateByte(probOfMutation,b)).map(ByteVector(_))
     }
 }
 
